0ee223bfa6832f881f65a5f24194d5fa4615cc46
jake trower <cakeandturtles@gmail.com>
2016-05-03 16:36:28 -0500

    1 robot launch file working. prepping for two.

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 - [x] fix transform for fake amcl
 - [x] write transform for apriltag for homework5 explanation
 - [x] solve restarting exploration (by dummying an interrupt on simulation)
+- [ ] get simulation to work with two robots...
 - [ ] dummy out a finder that works with another robot
 - [ ] related work (read and summarize academically)
 - [ ] results from first iteration (get data? time to find?? distance/efficiency of hiding??)
--- nodes/fake_amcl.py
+++ nodes/fake_amcl.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 import rospy
-
 import tf
 import math
 from nav_msgs.msg import Odometry
+import sys
 
-def odometryHandler(msg):
+started_broadcasting = False
+robot_num = 0
+
+def sendTransform(msg, base_link_frame_name):
     #this seems silly, but for whatever reason amcl is not broadcasting transform from base_link to map
     #so since the pose of the robot in simulation matches the map frame,
     #let's just force publish a tf!!
@@ -20,10 +23,22 @@ def odometryHandler(msg):
                      #tf.transformations.quaternion_from_euler(0, 0, math.radians(180)),
                      (ori.x, -ori.y, -ori.z, ori.w),
                      rospy.Time.now(),
-                     "map",
-                     "base_link")
+                     "/map",
+                     base_link_frame_name)
+
+def robotPoseHandler(msg):
+    global started_broadcasting, robot_num
+
+    sendTransform(msg, "robot_"+str(robot_num)+"/base_link")
+
+    if not started_broadcasting:
+        started_broadcasting = True
+        print "\tamcl fake broadcast for robot", robot_num, "started!"
 
 if __name__ == '__main__':
+    if len(sys.argv) == 2:
+        robot_num = int(sys.argv[1])
+
     rospy.init_node('fake_amcl')
-    rospy.Subscriber('/pose', Odometry, odometryHandler)
+    rospy.Subscriber('/robot_'+str(robot_num)+'/odom', Odometry, robotPoseHandler)
     rospy.spin()
